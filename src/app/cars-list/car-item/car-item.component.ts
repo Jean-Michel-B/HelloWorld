c8ebeade15869811a3fdad42d4ca94c8f940f045
Amar2050 <amarboutayeb@gmail.com>
2021-08-03 15:07:22 +0200

    Mini TP - Car Item -

--- src/app/cars-list/car-item/car-item.component.ts
+++ src/app/cars-list/car-item/car-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car-item',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarItemComponent implements OnInit {
 
+
+  @Input() vehicule:any;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
+  game:any = {
+    title: "Jeux d'echec",
+    joueurs: 2
+  }
+
+  vehiculeOne:any = {
+    name: "Mercedes 6.3 AMD",
+    price: 33000
+  }
+  vehiculeTwo:any = {
+    name: "Peugeot 80ti RTX",
+    price: 3500
+  }
+  vehiculeThree:any = {
+    name: "Citroën Ryzen",
+    price: 2050
+  }
+
   constructor() { }
 
   ngOnInit(): void {
