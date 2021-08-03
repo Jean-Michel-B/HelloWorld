664c80e2086e03d18d5fc2933167404c042cad23
Amar2050 <amarboutayeb@gmail.com>
2021-08-03 10:02:14 +0200

    Interpolation exemple

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
+  // 1er exemple d'interpolation
+  // Type string int ou n'importe
+  mardiMatin:any = "Ils ont l'air bien OFF";
+
+  // 2eme exemple objet ou propriete
+  game: any =  {
+
+    title:"Among Us",
+    support: "Android",
+    multi: 10
+
+  };
+  
+
+  age:number = 13 ;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Intepolation du retour d'une methode
+  getStock(){
+    return 2050;
+  }
+
+  // Appel a une methode pour toutes sortes de traitements
+  getWakeUp(){
+    console.log("Silence de cathédrale Andalou")
   }
 
 }
