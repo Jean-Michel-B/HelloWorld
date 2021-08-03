31d9268c1a26f77143d0c391b6a4ca3d58a44bfa
Amar2050 <amarboutayeb@gmail.com>
2021-08-03 10:39:59 +0200

    Property Binding exemple

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsListComponent implements OnInit {
 
+
+  coverImage:string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyf6QztqepSgrVR_tiqX96bcNQz1E6AytTH99zqr6fvAGSP0X45yfcuh-1EIVv2lsAwEU&usqp=CAU"
+
+
+
   // 1er exemple d'interpolation
   // Type string int ou n'importe
-  mardiMatin:any = "Ils ont l'air bien OFF";
+  mardiMatin:any = "Ils ont l'air bien OFF"
 
   // 2eme exemple objet ou propriete
   game: any =  {
@@ -18,10 +23,12 @@ export class CarsListComponent implements OnInit {
     support: "Android",
     multi: 10
 
-  };
+  }
+
+  userEmail="jeanmichel@symfonix.com"
   
 
-  age:number = 13 ;
+  age:number = 13 
 
   constructor() { }
 
@@ -31,6 +38,10 @@ export class CarsListComponent implements OnInit {
   // Intepolation du retour d'une methode
   getStock(){
     return 2050;
+  }
+
+  isNotAvailable(){
+    return true;
   }
 
   // Appel a une methode pour toutes sortes de traitements
