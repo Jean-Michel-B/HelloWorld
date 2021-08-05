3666676ae687426a8c43cc885286f987c79cb24c
Amar2050 <amarboutayeb@gmail.com>
2021-08-05 11:24:10 +0200

    ngStyle exemple

--- src/app/drivers-list/drivers-list.component.ts
+++ src/app/drivers-list/drivers-list.component.ts
@@ -8,12 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class DriversListComponent implements OnInit {
 
 
-  games:string[] = ["Halo Infinite", "Gow 5", "Forza 12", "States Of Decay 2", "Ori"];
+  colorOne:string = "blue";
 
-  constructor() { }
+  onOff:boolean= true;
 
+  marginPts:string = "50px";
+
+  
   ngOnInit(): void {
   }
+  constructor() { }
 
+  getCouleur(){
+    return "green";
+  }
 
+  onSwitch(){
+    if(this.onOff)
+      this.onOff = false;
+    else 
+      this.onOff= true;
+    
+   
+  }
+
+  // condition ? "dans ce cas" : "dans l'autre cas"
+truc(){
+  if (true) 
+    return "dans ce cas"
+  else 
+    return "dans l'autre cas"}
 }
